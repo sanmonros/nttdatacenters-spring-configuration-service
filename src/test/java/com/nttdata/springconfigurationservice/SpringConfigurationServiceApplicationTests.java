7dace136449f4ad278f26900d348d947f1349d52
Santiago Montaño Rosario <smontanr@emeal.nttdata.com>
2023-01-26 09:01:35 +0100

    Primer Commit Config Server

---
+++ src/test/java/com/nttdata/springconfigurationservice/SpringConfigurationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.springconfigurationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringConfigurationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
